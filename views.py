c69a1d3557a4920e8432730afffc04fb0cea0182
library <library@gmail.com>
2021-08-31 14:37:57 +0300

    a

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__, template_folder='../templates')
+views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
